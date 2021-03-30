9c71e762846d450c7029762d743a9207de26fc6f
mihrimahsenakucuk <73707051+mihrimahsenakucuk@users.noreply.github.com>
2021-03-30 21:01:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Atm atm = new Atm();
-
+        //Kullanıcı oluşturuldu.
         Hesap hesap = new Hesap("Sena Küçük","12345",5000);
 
         atm.calis(hesap);
